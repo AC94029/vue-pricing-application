151af53be588863651772f6897e843ed3666466e
ac94029 <nikitha.kaluvagadda@lumen.com>
2022-10-11 19:54:12 +0530

    Vue-Pricing_application

---
+++ src/components/Pricing.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container py-3">
+    <Header/>
+    <Main/>
+    <Footer/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Footer from './Footer.vue';
+import Header from './Header.vue';
+import Main from './Main.vue';
+
+export default defineComponent({
+  name: 'Pricing',
+  components: {
+    Footer,
+    Header,
+    Main
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/bootstrap.min.css(original).css'
+import './assets/style.css'
 
 createApp(App).mount('#app')
